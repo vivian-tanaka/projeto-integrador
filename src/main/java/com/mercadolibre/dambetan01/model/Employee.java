bef90d1f82a27be567705330e1f420acf041f9b5
matheusmeli <matheus.agomes@mercadolivre.com>
2021-07-01 18:09:37 -0300

    US01 - Added token authorization and Roles

--- src/main/java/com/mercadolibre/dambetan01/model/Employee.java
+++ src/main/java/com/mercadolibre/dambetan01/model/Employee.java
@@ -1,9 +1,19 @@
 package com.mercadolibre.dambetan01.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -11,7 +21,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public class Employee implements Serializable {
+public class Employee implements Serializable, UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +34,40 @@ public class Employee implements Serializable {
     @ManyToOne
     @JoinColumn(name = "warehouse_id")
     private Warehouse warehouse;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "employee_role",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/mercadolibre/dambetan01/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.mercadolibre.dambetan01.repository;
+
+import com.mercadolibre.dambetan01.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Employee findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+}
