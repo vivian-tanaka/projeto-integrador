b52948348087f25fa0ab7480e10cdcbac7f88302
matheusmeli <matheus.agomes@mercadolivre.com>
2021-07-07 11:49:59 -0300

    Logic to authenticate based on User entity

--- src/main/java/com/mercadolibre/projetointegrador/repository/UserRepository.java
+++ src/main/java/com/mercadolibre/projetointegrador/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.mercadolibre.projetointegrador.repository;
 
 import com.mercadolibre.projetointegrador.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
