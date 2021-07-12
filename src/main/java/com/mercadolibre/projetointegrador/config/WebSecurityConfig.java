fe063b85d23978a8204d9daac240d7bb2e43c548
Rayan Salewski <rayan.salewski@mercadolivre.com>
2021-07-12 12:00:32 -0300

    Update WebSecurityConfig.java

--- src/main/java/com/mercadolibre/projetointegrador/config/WebSecurityConfig.java
+++ src/main/java/com/mercadolibre/projetointegrador/config/WebSecurityConfig.java
@@ -25,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/api/v1/fresh-products/orders").hasAnyAuthority("ROLE_BUYER", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/v1/fresh-products/orders").hasAnyAuthority("ROLE_BUYER", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/v1/fresh-products/section").hasAnyAuthority("ROLE_SUPERVISOR", "ROLE_ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/v1/fresh-products/warehouse").hasAnyAuthority("ROLE_EMPLOYEE","ROLE_SUPERVISOR", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/v1/fresh-products/duedate").hasAnyAuthority("ROLE_SUPERVISOR", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/v1/fresh-products/duedate/list").hasAnyAuthority("ROLE_SUPERVISOR", "ROLE_ADMIN")
 
